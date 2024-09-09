693e65c70894c912873f9c0550b5df676f239b04
Black Helios Dev <blackheliosdev@gmail.com>
2024-09-09 04:25:01 +0100

    Update start.sh

--- lib/start.sh
+++ lib/start.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 while true
 do
-echo "Starting Asta-Md!"
-node .
+  echo -e "${GREEN}Starting Black Helios bot!${NC}"
+  
+  node .
+
+  echo -e "${GREEN}Black Helios bot stopped. Restarting...${NC}"
+
+  sleep 5
 done
